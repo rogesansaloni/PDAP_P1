88f1093831a3d9c983ca90c14f467f21507cbc2c
Sandra Cea <sandra.cea@students.salle.url.edu>
2020-12-01 18:02:06 +0100

    Update organize_dataset.m

--- organize_dataset.m
+++ organize_dataset.m
@@ -2,3 +2,6 @@
 % una funció de tipus script de Matlab que anomenareu organize_dataset.m, i que s’encarregarà
 % d’automatitzar el procés d’assignar els fitxers d’àudio en subcarpetes (1 subcarpeta per
 % categoria)
+
+
+%prueba sandra
