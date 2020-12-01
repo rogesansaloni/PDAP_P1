2f264ba2aefdedf3ebe133bcea230bb7da840a28
Sofía Bonifasi Asturias <sofia.bonifasi@students.salle.url.edu>
2020-12-01 18:05:45 +0100

    Delete organize_dataset.m

--- organize_dataset.m
+++
@@ -1,4 +0,0 @@
-% Per poder preparar la base de dades per treballar adequadament és imprescindible que genereu
-% una funció de tipus script de Matlab que anomenareu organize_dataset.m, i que s’encarregarà
-% d’automatitzar el procés d’assignar els fitxers d’àudio en subcarpetes (1 subcarpeta per
-% categoria)
