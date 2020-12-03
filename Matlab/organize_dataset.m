f52b7c23a572284feae0095cee497d66af2dfadc
rogesansaloni <rogelio.sansaloni@students.salle.url.edu>
2020-12-03 19:04:24 +0100

    Creada la función searchForErrors

--- Matlab/organize_dataset.m
+++ Matlab/organize_dataset.m
@@ -1,8 +1,18 @@
 %leemos el archivo meta.txt y error.txt
 meta = readtable('..\Ficheros\meta.txt','Delimiter','\t','ReadVariableNames',false);
-error = readtable('..\Ficheros\error.txt','Delimiter','\t','ReadVariableNames',false);
+errors = readtable('..\Ficheros\error.txt','Delimiter','\t','ReadVariableNames',false);
 %obtenemos todas las categorías
 categories = unique(meta{:,2});
-for 
-filename = cell2mat(T{r,1});
-category = cell2mat(T{r,2});
+mkdir ../Ficheros Categorias
+for i = 1:length(categories)
+    mkdir ('../Ficheros/Categorias',categories{i, 1});
+end
+for i = 1:length(meta)
+    audio_name = meta(i).Var1;
+    dir audio_name.m
+    if audio_name = 
+end
+dir ../Ficheros/Audios/audios1
+%for i = 1:
+%filename = cell2mat(T{r,1});
+%category = cell2mat(T{r,2});
---
+++ Matlab/searchForErrors.m
@@ -0,0 +1,9 @@
+function error = searchForErrors(filename,errors)
+%Esta función comprueba si el archivo de audio se encuentra dentro de los
+%archivos que tienen algún error
+for i = 1:length(erros)
+    name_audio = erros(i).Var1
+    if filename 
+end
+end
+
