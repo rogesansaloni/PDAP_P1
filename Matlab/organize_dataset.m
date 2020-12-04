8a817de16c56a0cbc9fde0c2849be82f058c0b78
rogesansaloni <rogelio.sansaloni@students.salle.url.edu>
2020-12-04 16:27:13 +0100

    Ahora se pasa el audio a mono

--- Matlab/organize_dataset.m
+++ Matlab/organize_dataset.m
@@ -14,10 +14,12 @@ for i = 1:meta_rows
     if ~(searchForErrors(audio_name, errors))
         new_audio_name = strrep(audio_name,"audio/","");
         old_path = strcat("../Ficheros/Audios/",new_audio_name);
-        audio_category = cell2mat(meta{i,2});
-        new_path = strcat("../Ficheros/Categorias/",audio_category);
         if isfile(old_path)
-            movefile(old_path, new_path);
+            audio_category = cell2mat(meta{i,2});
+            [x,Fs] = audioread(old_path);
+            x = 0.5*sum(x,2);
+            new_path = strcat("../Ficheros/Categorias/",audio_category,"/",new_audio_name);
+            audiowrite(new_path,x,Fs);
         end
     end
         
