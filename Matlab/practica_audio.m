c2a2198e369c03808bfd53c92e0d1664b2d4685a
rogesansaloni <rogelio.sansaloni@students.salle.url.edu>
2020-12-11 10:08:35 +0100

    El audioDatastore ya funcione

--- Matlab/practica_audio.m
+++ Matlab/practica_audio.m
@@ -1,4 +1,4 @@
-ads = audioDatastore('../PDAP_P1/Ficheros/Categorias','IncludeSubfolders',true,'FileExtensions','.wav', 'LabelSource','foldernames');
-% adsTall = tall(ads);
-% adsTall1 = tall(ads1);
-% [ads1, ads2, ads3, ads4] = splitEachLabel(ads,0.25,0.25,0.25);
+ads = audioDatastore('../Ficheros/Categorias','IncludeSubfolders',true,'FileExtensions','.wav', 'LabelSource','foldernames');
+adsTall = tall(ads);
+[ads1, ads2, ads3, ads4, ads5, ads6, ads7, ads8, ads9, ads10] = splitEachLabel(ads,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1);
+
