87408882afc6e3d37d795751cce9c3421e7d726f
rogesansaloni <rogelio.sansaloni@students.salle.url.edu>
2020-12-13 13:03:07 +0100

    Cambios en GMM, corregido el length

--- Matlab/GMM.m
+++ Matlab/GMM.m
@@ -1,10 +1,10 @@
 function ypred = GMM(X, categories, k)
 %Esta función aplica el clasificador GMM.
 %Para cada categoria se crea un modelo gmmModel
-gmmModel = cell (1, categories.length);
-gmmPDF = cell (1, categories.length);
+gmmModel = cell(1, length(categories));
+gmmPDF = cell(1, length(categories));
 x = 0:.1:1;
-    for i = 1: categories.length
+    for i = 1: length(categories)
         gmmModel{i} = fitgmdist(X,k);
         gmmPDF{i} = pdf(gmmModel, x);
     end
