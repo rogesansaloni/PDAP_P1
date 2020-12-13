fc97ac8ab8b21e482688321ce73c6aaa1d0e63f8
rogesansaloni <rogelio.sansaloni@students.salle.url.edu>
2020-12-13 12:54:16 +0100

    Añadida la funcion extractFeaturesLabels

--- Matlab/practica_audio.m
+++ Matlab/practica_audio.m
@@ -1,5 +1,9 @@
 %Paso 1
 ads = audioDatastore('../Ficheros/Categorias','IncludeSubfolders',true,'FileExtensions','.wav', 'LabelSource','foldernames');
 adsTall = tall(ads);
-[ads1, ads2, ads3, ads4, ads5, ads6, ads7, ads8, ads9, ads10] = splitEachLabel(ads,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1,0.1);
-
+%Difinimos en cuantas n partes dividiremos la informacion
+n=10;
+p=1/n;
+[ads1, ads2, ads3, ads4, ads5, ads6, ads7, ads8, ads9, ads10] = splitEachLabel(ads,p,p,p,p,p,p,p,p,p);
+adsSplit = {ads1, ads2, ads3, ads4, ads5, ads6, ads7, ads8, ads9, ads10};
+[features, labels] = extractFeaturesLabels(adsSplit{1,1});
