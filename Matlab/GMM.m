b9d0ae26b58fe0450240aa109ecc403ec633ed6f
rogesansaloni <rogelio.sansaloni@students.salle.url.edu>
2020-12-17 00:55:53 +0100

    Pequeño cambio en GMM

--- Matlab/GMM.m
+++ Matlab/GMM.m
@@ -2,8 +2,8 @@ function ypred = GMM(learnDB, learnGT, testDB)
 %Esta función aplica el clasificador GMM.
 %Para cada categoria se crea un modelo gmmModel
 categories = unique(learnGT);
-numCategories = size(categories);
-k = 2;
+numCategories = length(categories);
+k = numCategories;
 gmmModel = cell(1, length(numCategories));
 gmmPDF = cell(1, length(numCategories));
 for i = 1: numCategories
@@ -11,10 +11,10 @@ for i = 1: numCategories
     positions = find(learnGT == name_category);
     data_category = [];
     for n = 1:size(positions)
-        data_category = [data_category;learnDB(positions(n))];
+        data_category = [data_category;learnDB(positions(n),:)];
     end
     gmmModel{i} = fitgmdist(data_category,k);
-    gmmPDF{i} = pdf(gmmModel{i}, testDB);
+    gmmPDF{i} = pdf(gmmModel{i}, testDB(:));
 end
 ypred = max(gmmPDF);
 %disp('GMM-> maxima probabilidad ', ypred);
