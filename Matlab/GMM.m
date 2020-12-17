4ed8dff70e090a4ac3885fdb998d5599b720d84f
rogesansaloni <rogelio.sansaloni@students.salle.url.edu>
2020-12-17 22:45:14 +0100

    HELP

--- Matlab/GMM.m
+++ Matlab/GMM.m
@@ -1,5 +1,4 @@
-
-function [ypred,gmmPDF] = GMM(learnDB, learnGT, testDB, k)
+function ypred = GMM(learnDB, learnGT, testDB, k)
 %Esta función aplica el clasificador GMM.
 %Para cada categoria se crea un modelo gmmModel
 categories = unique(learnGT);
