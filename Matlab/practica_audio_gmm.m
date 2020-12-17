0fb56243c88e9344a2ff3910e94e78c98b048f94
rogesansaloni <rogelio.sansaloni@students.salle.url.edu>
2020-12-18 00:41:00 +0100

    practica_audio final

--- Matlab/practica_audio_gmm.m
+++ Matlab/practica_audio_gmm.m
@@ -74,3 +74,37 @@ for k = 1:4
     end
 end
 accuracy_mean = mean(accuracy);
+
+function [features,labels] = extractFeaturesLabels(ads)
+    %Esta función se encarga de dividir la información del ads en 4 partes,
+    %obteniendo los cell arrays y los vectores de categories de cada parte, y
+    %creando el array features y labels con estos datos, respectivamente.
+    [ads1, ads2, ads3, ads4] = splitEachLabel(ads,0.25,0.25,0.25);
+    adsSplit = {ads1, ads2, ads3, ads4};
+    features = cell(1,4);
+    labels = cell(1,4);
+    aFE = audioFE();
+    for i = 1:4
+        ads = adsSplit{i};
+        adsTall = tall(ads);
+        featuresTall = cellfun(@(x)extract(aFE,x),adsTall, "UniformOutput", false);
+        features{i} = cell2mat(gather(featuresTall));
+        
+        labels{i} = ads.Labels;
+    end
+end
+
+function aFE = audioFE ()
+    %Usada para definir los atributos que analizaremos de los audios
+    fs = 44100; 
+    %Se definen los valores de longitud de la trama y numero de
+    %muestras solapadas 
+    aFE = audioFeatureExtractor(...
+    "SampleRate",fs, ...
+    "Window",hamming(round(0.6*fs),"periodic"), ...
+    "OverlapLength",round(0.5*fs), ...
+    "mfcc",true, ...
+    "gtcc",true, ...
+    "pitch",true, ...
+    "spectralCentroid",true);
+end
